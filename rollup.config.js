6c438c620d40d33eb0e55368f8477d3c236efaab
Mike Lerner <mlerner@fourwindsinteractive.com>
2019-02-04 17:10:31 -0700

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from 'rollup-plugin-babel';
+import multiEntry from 'rollup-plugin-multi-entry';
+
+export default [
+    {
+        entry: 'src/*.js',
+        dest: 'dist/fwi.es.js',
+        format: 'es',
+        plugins: [multiEntry()]
+    },
+    {
+        entry: 'src/*.js',
+        dest: 'dist/fwi.common.js',
+        format: 'cjs',
+        plugins: [multiEntry(), babel({ plugins: ['external-helpers'] })],
+        moduleName: 'fwi'
+    },
+    {
+        entry: 'src/*.js',
+        dest: 'dist/fwi.browser.js',
+        format: 'iife',
+        plugins: [multiEntry(), babel()],
+        moduleName: 'fwi'
+    }
+];
+
+// export default {
+//     input: 'src/*.js',
+//     plugins: [multiEntry()],
+//     output: {
+//         file: 'dist/fwi.js',
+//         format: 'cjs'
+//     }
+// }
